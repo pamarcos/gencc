e4d95d72090126c65123704634a97ac1c0fb92a8
Pablo Marcos Oltra <pablo.marcos.oltra@gmail.com>
2016-11-23 21:13:34 +0000

    Update MockHelper to comply with new prototype

--- tests/unit_tests/mock_helper.h
+++ tests/unit_tests/mock_helper.h
@@ -9,7 +9,12 @@ public:
     MOCK_CONST_METHOD2(getEnvVar, bool(const char*, std::string&));
     MOCK_CONST_METHOD2(setEnvVar, void(const char*, const std::string&));
     MOCK_METHOD1(getCwd, bool(std::string&));
-    MOCK_METHOD1(runCommand, int(const std::string& str));
+    MOCK_CONST_METHOD1(runCommand, int(const std::string& str));
+    MOCK_CONST_METHOD1(msleep, void(int));
+    MOCK_CONST_METHOD1(fileExists, bool(const std::string& filename));
+    MOCK_CONST_METHOD1(getFileLock, std::unique_ptr<FileLock>(const std::string& filename));
+    MOCK_CONST_METHOD1(getFileIstream, std::unique_ptr<std::istream>(const std::string& filename));
+    MOCK_CONST_METHOD1(getFileOstream, std::unique_ptr<std::ostream>(const std::string& filename));
 };
 
 #endif // MOCK_HELPER_H
