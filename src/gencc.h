0047b6dff6884ccd180c9a255d156313fdc6b753
Pablo Marcos Oltra <pablo.marcos.oltra@gmail.com>
2016-11-28 18:58:20 +0000

    Add GPLv3 license for gencc and 3rd party licenses

--- src/gencc.h
+++ src/gencc.h
@@ -1,3 +1,24 @@
+/**
+ * gencc is an application that generates compilation databases for clang
+ *
+ * Copyright (C) 2016 Pablo Marcos Oltra
+ *
+ * This file is part of gencc.
+ *
+ * gencc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gencc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef GENCC_H
 #define GENCC_H
 
